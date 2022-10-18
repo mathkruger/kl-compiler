cf731ab80d4dcdff57ac897c7095bd7704321231
Matheus da Silva Kruger <ms-kruger@hotmail.com>
2022-10-17 23:06:16 -0300

    using RegExp

--- src/compiler/functions/parse/parse.js
+++ src/compiler/functions/parse/parse.js
@@ -3,65 +3,66 @@ module.exports = (klCode) => {
     if (code == null) return "";
 
     // Variable structures
-    code = code.replace(/(KASINO)(?=(?:[^"]|"[^"]*")*$)/g, 'let');
-    code = code.replace(/(KASINAE)(?=(?:[^"]|"[^"]*")*$)/g, 'var');
-    code = code.replace(/(KASINAO)(?=(?:[^"]|"[^"]*")*$)/g, 'const');
+    code = code.replace(new RegExp("(KASINO)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'let');
+    code = code.replace(new RegExp("(KASINAE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'var');
+    code = code.replace(new RegExp("(KASINAO)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'const');
 
-    code = code.replace(/(->)(?=(?:[^"]|"[^"]*")*$)/g, '.');
+    code = code.replace(new RegExp("(->)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '.');
 
     // modules
-    code = code.replace(/(EXPORTAE)(?=(?:[^"]|"[^"]*")*$)/g, 'module.exports');
-    code = code.replace(/(REQUISITAE)(?=(?:[^"]|"[^"]*")*$)/g, 'require');
+    code = code.replace(new RegExp("(EXPORTAE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'module.exports');
+    code = code.replace(new RegExp("(REQUISITAE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'require');
 
-    code = code.replace(/(CALMAE)(?=(?:[^"]|"[^"]*")*$)/g, 'async');
-    code = code.replace(/(ESPERAE)(?=(?:[^"]|"[^"]*")*$)/g, 'await');
+    code = code.replace(new RegExp("(CALMAE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'async');
+    code = code.replace(new RegExp("(ESPERAE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'await');
 
     // Output
-    code = code.replace(/(VAIDJ)(?=(?:[^"]|"[^"]*")*$)/g, 'console.log');
+    code = code.replace(new RegExp("(VAIDJ)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'console.log');
 
     // Operators
-    code = code.replace(/(É)(?=(?:[^"]|"[^"]*")*$)/g, '=');
-    code = code.replace(/(MAIOR)(?=(?:[^"]|"[^"]*")*$)/g, '>');
-    code = code.replace(/(MENOR)(?=(?:[^"]|"[^"]*")*$)/g, '<');
-    code = code.replace(/(NAUM)(?=(?:[^"]|"[^"]*")*$)/g, '!');
-    code = code.replace(/(GILBARROWS)(?=(?:[^"]|"[^"]*")*$)/g, '=>');
-    code = code.replace(/(VERDADE)(?=(?:[^"]|"[^"]*")*$)/g, 'true');
-    code = code.replace(/(FALSO)(?=(?:[^"]|"[^"]*")*$)/g, 'false');
-    code = code.replace(/(SOMAE)(?=(?:[^"]|"[^"]*")*$)/g, '+');
-    code = code.replace(/(MENOSAE)(?=(?:[^"]|"[^"]*")*$)/g, '-');
+    code = code.replace(new RegExp("(É)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '=');
+    code = code.replace(new RegExp("(ÉÉ)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '==');
+    code = code.replace(new RegExp("(MAIOR)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '>');
+    code = code.replace(new RegExp("(MENOR)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '<');
+    code = code.replace(new RegExp("(NAUM)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '!');
+    code = code.replace(new RegExp("(GILBARROWS)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '=>');
+    code = code.replace(new RegExp("(VERDADE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'true');
+    code = code.replace(new RegExp("(FALSO)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'false');
+    code = code.replace(new RegExp("(SOMAE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '+');
+    code = code.replace(new RegExp("(MENOSAE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '-');
 
 
     // Conditional
-    code = code.replace(/(DESTAQUE)(?=(?:[^"]|"[^"]*")*$)/g, 'if');
-    code = code.replace(/(INTERNACIONAL)(?=(?:[^"]|"[^"]*")*$)/g, 'else');
+    code = code.replace(new RegExp("(DESTAQUE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'if');
+    code = code.replace(new RegExp("(INTERNACIONAL)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'else');
 
     // Symbols
-    code = code.replace(/(BOA NOITE)(?=(?:[^"]|"[^"]*")*$)/g, '{');
-    code = code.replace(/(BOA TARDE)(?=(?:[^"]|"[^"]*")*$)/g, '}');
+    code = code.replace(new RegExp("(BOA NOITE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '{');
+    code = code.replace(new RegExp("(BOA TARDE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '}');
 
     // Switch-case structure
-    code = code.replace(/(CAN'T)(?=(?:[^"]|"[^"]*")*$)/g, 'switch');
-    code = code.replace(/(GET)(?=(?:[^"]|"[^"]*")*$)/g, 'case');
-    code = code.replace(/(OVER)(?=(?:[^"]|"[^"]*")*$)/g, 'break');
-    code = code.replace(/(BABY)(?=(?:[^"]|"[^"]*")*$)/g, 'default');
+    code = code.replace(new RegExp("(CAN'T)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'switch');
+    code = code.replace(new RegExp("(GET)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'case');
+    code = code.replace(new RegExp("(OVER)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'break');
+    code = code.replace(new RegExp("(BABY)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'default');
 
     // For loop
-    code = code.replace(/(AS BALADAS)(?=(?:[^"]|"[^"]*")*$)/g, 'for');
-    code = code.replace(/(VAI)(?=(?:[^"]|"[^"]*")*$)/g, 'continue');
-    code = code.replace(/(ARREBENTA)(?=(?:[^"]|"[^"]*")*$)/g, 'return');
+    code = code.replace(new RegExp("(AS BALADAS)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'for');
+    code = code.replace(new RegExp("(VAI)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'continue');
+    code = code.replace(new RegExp("(ARREBENTA)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'return');
 
     // function
-    code = code.replace(/(CHAMA)(?=(?:[^"]|"[^"]*")*$)/g, '');
-    code = code.replace(/(SABADAÇO)(?=(?:[^"]|"[^"]*")*$)/g, 'function');
+    code = code.replace(new RegExp("(CHAMA)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '');
+    code = code.replace(new RegExp("(SABADAÇO)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), 'function');
 
     // delimitadores
-    code = code.replace(/(ABRE)(?=(?:[^"]|"[^"]*")*$)/g, '(');
-    code = code.replace(/(FECHA)(?=(?:[^"]|"[^"]*")*$)/g, ')');
+    code = code.replace(new RegExp("(ABRE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '(');
+    code = code.replace(new RegExp("(FECHA)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), ')');
 
     // comentários
-    code = code.replace(/(\/COMENTAE)(?=(?:[^"]|"[^"]*")*$)/g, '//');
-    code = code.replace(/(\/COMENTASSO)(?=(?:[^"]|"[^"]*")*$)/g, '/*');
-    code = code.replace(/(COMENTASSO\/)(?=(?:[^"]|"[^"]*")*$)/g, '*/');
+    code = code.replace(new RegExp("(\/COMENTAE)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '//');
+    code = code.replace(new RegExp("(\/COMENTASSO)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '/*');
+    code = code.replace(new RegExp("(COMENTASSO\/)(?=(?:[^\"]|\"[^\"]*\")*$)", "g"), '*/');
 
     return code;
 }
